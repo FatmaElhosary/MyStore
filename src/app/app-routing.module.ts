897ef706272863874099247c385f4924cdd0742e
Fatma <engfatmafathy@gmail.com>
2022-07-20 10:40:10 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { CartComponent } from './components/cart/cart.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'products/:id', component: ProductItemDetailComponent },
+  { path: 'confirm', component: ConfirmationComponent },
+  {path:'cart',component:CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/product-item/product-item.component.ts
@@ -0,0 +1,24 @@
+import { Product } from './../../models/Product';
+import { Component, OnInit ,Input} from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+@Input() product:Product;
+  constructor() {
+    this.product={
+      id:0,
+      name:'',
+      price:0,
+      url:'',
+      description:''
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
